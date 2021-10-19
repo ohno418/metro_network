5d96ec9d73955d71bc67adc83d0960fc362fafa5
taro0 <yutaro.ono.418@gmail.com>
2021-10-20 00:44:53 +0900

    Add a ekikan type

---
+++ ekikan.ml
@@ -0,0 +1,8 @@
+(* 駅間type *)
+type ekikan_t = {
+  kiten : string;  (* 起点の駅名 *)
+  shuten : string; (* 終点の駅名 *)
+  keiyu : string;  (* 経由駅の駅名 *)
+  kyori : int;     (* 距離 *)
+  jikan : int;     (* 所要時間 *)
+}
